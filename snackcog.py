740e085991729defbb1c80699bc9104c5f654660
TheAmmiR <yakorol2015@gmail.com>
2021-01-17 17:56:19 +0200

    Add new snack

---
+++ snackcog.py
@@ -0,0 +1,23 @@
+import discord
+import menu
+import random
+from discord.ext import commands
+
+class SnackCog(commands.Cog, name = 'Закуски'):
+	def __init__(self, bot):
+		self.bot = bot
+
+	@commands.command(name = 'парварда', aliases = ['parvarda'])
+	async def order_parvarda(self, ctx):
+		embed = discord.Embed(
+				colour = 0x289566,
+				description = random.choice(menu.ORDER_PHRASES)
+			)
+		embed.set_image(url = random.choice(menu.PARVARDA_PICS))
+		embed.set_footer(text = random.choice(menu.PARVARDA_FACTS))
+
+		await ctx.send(embed = embed)
+
+
+def setup(bot):
+	bot.add_cog(SnackCog(bot))
